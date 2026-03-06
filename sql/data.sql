caa9dd6940873ab8622c03ffd412d6c42152ef7d
Ricah3510 <tendryandria6@gmail.com>
2026-03-06 11:24:59 +0300

    [initialisation] [Correction] script

---
+++ sql/data.sql
@@ -0,0 +1,38 @@
+INSERT INTO t_correcteur (nom) VALUES
+('Correcteur 1'),
+('Correcteur 2'),
+('Correcteur 3'),
+('Correcteur 4');
+
+INSERT INTO t_matiere (nom) VALUES
+('Mathematiques'),
+('Anglais');
+
+INSERT INTO t_candidat (nom) VALUES
+('Candidat 1'),
+('Candidat 2');
+
+INSERT INTO t_resolution (nom) VALUES
+('plus petit'),
+('plus grand'),
+('moyenne');
+
+INSERT INTO t_operateur (operateur) VALUES
+('<'),
+('>');
+
+INSERT INTO t_parametre (id_matiere, diff, id_operateur, id_resolution) VALUES
+(1, 5, 1, 2),
+(1, 5, 2, 1),
+(2, 4, 1, 2),
+(2, 4, 2, 1);
+
+INSERT INTO t_note (id_candidat, id_matiere, id_correcteur, note) VALUES
+-- Candidat 1
+(1,1,1,7.5),
+(1,1,2,6),
+
+-- Candidat 2
+(2,2,1,8),
+(2,2,2,9),
+(2,2,3,13);

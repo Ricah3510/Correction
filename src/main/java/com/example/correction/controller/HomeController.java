f57bc49d6d9d555c34a74915945a3ccd60788d5b
Ricah3510 <tendryandria6@gmail.com>
2026-03-08 10:38:57 +0300

    [initialisation] [Correction] maj structure

--- src/main/java/com/example/correction/controller/HomeController.java
+++ src/main/java/com/example/correction/controller/HomeController.java
@@ -1,14 +1,39 @@
 package com.example.correction.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.correction.model.Candidat;
+import com.example.correction.service.CandidatService;
+
 
 @Controller
 public class HomeController {
 
+    private final CandidatService candidatService;
+
+    public HomeController(CandidatService candidatService) {
+        this.candidatService = candidatService;
+    }
     @GetMapping("/")
     public String home(){
         return "home";
     }
+
+    @GetMapping("/test")
+    public ModelAndView test() {
+
+        ModelAndView mv = new ModelAndView("test");
+
+        List<Candidat> listes = candidatService.findAll();
+
+        mv.addObject("listes", listes);
+
+        return mv;
+    }
+    
 
 }
--- src/main/java/com/example/correction/model/Note.java
+++ src/main/java/com/example/correction/model/Note.java
@@ -21,7 +21,67 @@ public class Note {
     @ManyToOne
     @JoinColumn(name = "id_correcteur")
     private Correcteur correcteur;
-
+    
     private double note;
+
+    public Note(Long id, Matiere matiere, Candidat candidat, Correcteur correcteur, double note) {
+        this.id = id;
+        this.matiere = matiere;
+        this.candidat = candidat;
+        this.correcteur = correcteur;
+        this.note = note;
+    }
+
+    
+
+    public Note(Matiere matiere, Candidat candidat, Correcteur correcteur, double note) {
+        this.matiere = matiere;
+        this.candidat = candidat;
+        this.correcteur = correcteur;
+        this.note = note;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Matiere getMatiere() {
+        return matiere;
+    }
+
+    public void setMatiere(Matiere matiere) {
+        this.matiere = matiere;
+    }
+
+    public Candidat getCandidat() {
+        return candidat;
+    }
+
+    public void setCandidat(Candidat candidat) {
+        this.candidat = candidat;
+    }
+
+    public Correcteur getCorrecteur() {
+        return correcteur;
+    }
+
+    public void setCorrecteur(Correcteur correcteur) {
+        this.correcteur = correcteur;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+    public void setNote(double note) {
+        this.note = note;
+    }
+    
 
 }
